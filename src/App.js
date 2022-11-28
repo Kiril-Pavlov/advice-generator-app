a5824dd29d32d8ffc2b8d9398f1b1cd50fb16128
Kiril-Pavlov <Kire_P@yahoo.com>
2022-11-28 19:55:39 +0100

    responsive done. App done

--- src/App.js
+++ src/App.js
@@ -3,18 +3,18 @@ import axios from 'axios';
 import { useEffect, useState } from 'react';
 
 import desktopDivider from "./assets/pattern-divider-desktop.svg"
-
+import mobileDivider from "./assets/pattern-divider-mobile.svg"
 import diceIcon from "./assets/icon-dice.svg"
 
 function App() {
-  const [id,setId]=useState("");
-  const [advice,setAdvice] = useState("");
+  const [id, setId] = useState("");
+  const [advice, setAdvice] = useState("");
 
-  useEffect(()=>{
+  useEffect(() => {
     getAdvice();
-  },[])
+  }, [])
 
-  const getAdvice = () =>{
+  const getAdvice = () => {
     axios.get("https://api.adviceslip.com/advice").then(res => {
       console.log(res.data.slip.id)
       setId(res.data.slip.id)
@@ -27,7 +27,10 @@ function App() {
     <div className="advice-container">
       <h6 className='advice-title'>ADVICE #{id}</h6>
       <div className='advice-content'>"{advice}"</div>
-      <img src={desktopDivider} alt="divider" className='divider-img'/>
+      <picture className='divider-img'>
+        <source media='(max-width:500px)' srcSet={mobileDivider} />
+        <img src={desktopDivider} alt="divider" className='divider-img' />
+      </picture>
       <button onClick={getAdvice} className="get-advice-btn"><img src={diceIcon} alt="dice" /></button>
     </div>
   );
