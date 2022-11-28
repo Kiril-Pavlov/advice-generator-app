ade4372c9cfd27f7b668e54cf0c833afd38fe8bd
Kiril-Pavlov <Kire_P@yahoo.com>
2022-11-28 18:52:31 +0100

    call to api working

--- src/App.js
+++ src/App.js
@@ -1,9 +1,19 @@
 import './App.css';
+import axios from 'axios';
 
 function App() {
+
+  const getAdvice = () =>{
+    axios.get("https://api.adviceslip.com/advice").then(res => {
+      console.log(res.data.slip.id)
+      console.log(res.data.slip.advice)
+    })
+  }
+
   return (
     <div className="App">
       <h1>Hi</h1>
+      <button onClick={getAdvice}>Get Advice</button>
     </div>
   );
 }
