f86a0be1184a8b2cbe26f189a9ecf14ec3a8f76d
Kiril-Pavlov <Kire_P@yahoo.com>
2022-11-28 19:44:14 +0100

    styles for desktop done

--- src/App.js
+++ src/App.js
@@ -1,19 +1,34 @@
 import './App.css';
 import axios from 'axios';
+import { useEffect, useState } from 'react';
+
+import desktopDivider from "./assets/pattern-divider-desktop.svg"
+
+import diceIcon from "./assets/icon-dice.svg"
 
 function App() {
+  const [id,setId]=useState("");
+  const [advice,setAdvice] = useState("");
+
+  useEffect(()=>{
+    getAdvice();
+  },[])
 
   const getAdvice = () =>{
     axios.get("https://api.adviceslip.com/advice").then(res => {
       console.log(res.data.slip.id)
+      setId(res.data.slip.id)
       console.log(res.data.slip.advice)
+      setAdvice(res.data.slip.advice)
     })
   }
 
   return (
-    <div className="App">
-      <h1>Hi</h1>
-      <button onClick={getAdvice}>Get Advice</button>
+    <div className="advice-container">
+      <h6 className='advice-title'>ADVICE #{id}</h6>
+      <div className='advice-content'>"{advice}"</div>
+      <img src={desktopDivider} alt="divider" className='divider-img'/>
+      <button onClick={getAdvice} className="get-advice-btn"><img src={diceIcon} alt="dice" /></button>
     </div>
   );
 }
